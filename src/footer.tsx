e2bfdf266ece61889d18c98414063c72cbd78198
Alex Ryse <aryse54@gmail.com>
2025-10-04 19:30:43 -0700

    Started udpating footer.

--- src/footer.tsx
+++ src/footer.tsx
@@ -6,13 +6,23 @@ function Footer() {
 
 	return (
 		<div className='footer-container'>
-			<p>&copy; 2023 LZM Landscaping LLC. All rights reserved.</p>
-			<a href="https://www.yelp.com/biz/lzm-landscaping-gig-harbor?osq=Lzm+Landscaping&override_cta=Request+pricing+%26+availability" target="_blank" rel="noopener noreferrer">
-				<FontAwesomeIcon icon={faYelp} />
-			</a>
-			<a href="https://www.facebook.com/p/LZM-Landscaping-LLC-61577894886146" target="_blank" rel="noopener noreferrer">
-				<FontAwesomeIcon icon={faFacebook} />
-			</a>
+			<div className='footer-left'>
+				<p>Outdoor Care Done Right | Since 2023</p>
+				<div>
+					<a href="https://www.yelp.com/biz/lzm-landscaping-gig-harbor?osq=Lzm+Landscaping&override_cta=Request+pricing+%26+availability" target="_blank" rel="noopener noreferrer">
+					<FontAwesomeIcon icon={faYelp} />
+					</a>
+					<a href="https://www.facebook.com/p/LZM-Landscaping-LLC-61577894886146" target="_blank" rel="noopener noreferrer">
+						<FontAwesomeIcon icon={faFacebook} />
+					</a>
+				</div>
+			</div>
+			<div className='footer-center'>
+				<p>&copy; 2023 LZM Landscaping LLC. All rights reserved.</p>
+			</div>
+			<div className='footer-right'>
+				<p>Nav Links Here</p>
+			</div>
 		</div>
 	);
 };
