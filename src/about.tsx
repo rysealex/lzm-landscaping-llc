3b2b07bb51b5853f4cbf83b3056884231dab727e
Alex Ryse <aryse54@gmail.com>
2025-10-07 23:30:39 -0700

    Added founder pic and added testimonials subsection in about section.

--- src/about.tsx
+++ src/about.tsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
+import luis from './luis.jpeg';
 import './App.css';
 
 function About() {
@@ -21,7 +24,7 @@ function About() {
 			<section className='about-section about-team'>
 				<h3>Meet Luis Zacarias, Founder</h3>
 				<div className='founder-profile'>
-					<img src='' alt='Luis Zacarias' className='founder-photo' />
+					<img src={luis} alt='Luis Zacarias' className='founder-photo' />
 					<div className='founder-bio'>
 						<p>
 							Luis Zacarias began his career in landscaping 10 years ago.
@@ -43,6 +46,43 @@ function About() {
 					<li>Quality: We use only the best materials and practices to ensure lasting results.</li>
 					<li>Customer Satisfaction: We work closely with our clients to bring their vision to life.</li>
 				</ul>
+			</section>
+			{/* Testimonials Section */}
+			<section className='about-section about-testimonials'>
+				<h3>Testimonials</h3>
+				<div className='testimonial'>
+					<div className='testimonial-icon-container'>
+						<FontAwesomeIcon icon={faQuoteLeft} className='testimonial-icon' />
+					</div>
+					<div className='testimonial-quote'>
+						<p>
+							LZM Landscaping transformed our backyard into a beautiful oasis. Highly recommend and will use them again!
+						</p>
+						<p className='testimonial-author'>- Sarah M.</p>
+					</div>
+				</div>
+				<div className='testimonial'>
+					<div className='testimonial-icon-container'>
+						<FontAwesomeIcon icon={faQuoteLeft} className='testimonial-icon' />
+					</div>
+					<div className='testimonial-quote'>
+						<p>
+							LZM Landscaping transformed our backyard into a beautiful oasis. Highly recommend and will use them again!
+						</p>
+						<p className='testimonial-author'>- Sarah M.</p>
+					</div>
+				</div>
+				<div className='testimonial'>
+					<div className='testimonial-icon-container'>
+						<FontAwesomeIcon icon={faQuoteLeft} className='testimonial-icon' />
+					</div>
+					<div className='testimonial-quote'>
+						<p>
+							LZM Landscaping transformed our backyard into a beautiful oasis. Highly recommend and will use them again!
+						</p>
+						<p className='testimonial-author'>- Sarah M.</p>
+					</div>
+				</div>
 			</section>
 		</div>
 	);
