f7d5a9b2e82280c8aa89f0751b5782a2b6097464
Alex Ryse <aryse54@gmail.com>
2025-11-01 20:31:37 -0700

    Scroll to top button.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleUp } from '@fortawesome/free-solid-svg-icons';
 import Navbar from './navbar';
 import Home from './home';
 import Services from './services';
@@ -9,11 +11,39 @@ import Footer from './footer';
 function App() {
 
   const [selectedService, setSelectedService] = useState<string | null>(null);
+  const [showScrollButton, setShowScrollButton] = useState(false);
 
   // function will be passed down to the Navbar
   const openServiceModal = (serviceName: string) => {
     setSelectedService(serviceName);
   };
+
+  // function to handle the scroll-to-top logic
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
+  useEffect(() => {
+    const scrollToTopButton = document.querySelector('.scroll-to-top') as HTMLElement;
+
+    const toggleScrollButtonVisibility = () => {
+      if (!scrollToTopButton) return;
+      // if scroll down 200px, show the button
+      if (window.scrollY > 200) {
+        setShowScrollButton(true);
+        scrollToTopButton.classList.add('show');
+      } else {
+        setShowScrollButton(false);
+        scrollToTopButton.classList.remove('show');
+      }
+    };
+
+    window.addEventListener('scroll', toggleScrollButtonVisibility);
+    return () => window.removeEventListener('scroll', toggleScrollButtonVisibility);
+  }, []);
 
   return (
     <div>
@@ -36,6 +66,13 @@ function App() {
       <section id="footer">
         <Footer />
       </section>
+      <button 
+        className={`scroll-to-top ${showScrollButton ? 'show' : ''}`}
+        onClick={scrollToTop}
+        type="button" 
+      >
+        <FontAwesomeIcon icon={faAngleUp} />
+      </button>
     </div>
   );
 }
